b1459fdfb5092caf8c11a9e45ba31eafceed33d9
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-03-28 08:56:41 +0200

    Refactor.

--- src/main/java/no/fint/model/resource/Link.java
+++ src/main/java/no/fint/model/resource/Link.java
@@ -1,15 +1,23 @@
 package no.fint.model.resource;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
-public class Link<T> {
+public class Link {
     @Getter
     private String href;
 
     public void setVerdi(String verdi) {
         href = verdi;
+    }
+
+    public static Link with(String verdi) {
+        return new Link(verdi);
     }
 }
 
