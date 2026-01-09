56a91aea4bd619821fcfc83106e1d0471ae51d3e
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2026-01-09 12:20:07 +0100

    Fix placeholder class Novari group id

--- src/main/java/no/novari/fint/model/resource/Link.java
+++ src/main/java/no/novari/fint/model/resource/Link.java
@@ -36,7 +36,7 @@ public class Link implements Serializable {
 
     public static String getHrefPlaceholder(Class<?> placeholderClass) {
         return placeholderClass.getName()
-                .replaceFirst("^no\\.fint\\.model(\\.resource)?\\.", "")
+                .replaceFirst("^no\\.novari\\.fint\\.model(\\.resource)?\\.", "")
                 .replaceFirst("Resource$", "")
                 .toLowerCase();
     }
